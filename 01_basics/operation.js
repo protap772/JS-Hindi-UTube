9bbc2703ca3a24ac0a3769fc362f5b5852e2f6e5
Protap Sen <155288969+protap772@users.noreply.github.com>
2025-10-05 18:58:07 +0000

    container config done

---
+++ 01_basics/operation.js
@@ -0,0 +1,34 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = "protap"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
