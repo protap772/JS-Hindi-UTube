aea61890d8a785249113994faf4cdd78ef0dcc07
Protap Sen <155288969+protap772@users.noreply.github.com>
2025-10-08 05:10:50 +0000

    config done

---
+++ 01_basics/strings.js
@@ -0,0 +1,24 @@
+const name = "protap"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('protappc')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString);
+
+const newStringOne = "   protap   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
