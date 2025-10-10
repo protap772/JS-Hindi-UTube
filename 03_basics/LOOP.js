4992477a0059211c18de8c801a730ba93be98478
Protap Sen <155288969+protap772@users.noreply.github.com>
2025-10-10 17:59:08 +0000

    config done by me

---
+++ 03_basics/LOOP.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 100000; i++) {
+    const element = i;
+    //if (element == 5) {
+        console.log("I love you ♥💖❤😘❤💖♥");
+    }
+   // console.log(element);
+    
+//}
+ //console.log(element);
